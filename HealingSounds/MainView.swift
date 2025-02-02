e269fba8a4988aead1268d7d7d46b47a2e2947f2
Ainur <sibrinat616@gmail.com>
2025-02-02 21:25:37 +0300

    try to make indicators

--- HealingSounds/MainView.swift
+++ HealingSounds/MainView.swift
@@ -12,36 +12,56 @@ struct MainView: View {
     
     var body: some View {
         VStack {
-            Text("Выберите звуковую\n терапию")
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color("TextColor"))
-                .font(.custom("MontserratAlternates-Regular", size: 18))
-            
+            HStack {
+                Spacer()
+                
+                Text("Выберите звуковую\n терапию")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("TextColor"))
+                    .font(.custom("MontserratAlternates-Regular", size: 18))
+                
+                Spacer()
+                
+                Circle()
+                    .foregroundColor(Color("RectangleColor"))
+                    .frame(width: 42, height: 42)
+                    .overlay(
+                        Image("lupaIcon")
+                        )
+            }
             ScrollViewReader { proxy in
-                           ScrollView(.horizontal) {
-                               HStack(spacing: 0) {
-                                   ForEach(0..<3, id: \.self) { index in
-                                       WaveRectangleView(title: "Альфа-волны", tag: "Сознательное", tag2: "Бодрствование", image: "WaveImage")
-                                       WaveRectangleView(title: "Альфа-волны", tag: "Сознательное", tag2: "Бодрствование", image: "WaveImage")
-                                       WaveRectangleView(title: "Альфа-волны", tag: "Сознательное", tag2: "Бодрствование", image: "WaveImage")
-                                           .id(index)
-                                   }
-                               }
-                               .scrollTargetLayout()
-                           }
-                           .scrollTargetBehavior(.paging)
-                           .scrollIndicators(.hidden)
-                           .onChange(of: currentPage) { _, newPage in
-                               withAnimation {
-                                   proxy.scrollTo(newPage, anchor: .center)
-                               }
-                           }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        ForEach(0..<3, id: \.self) { index in
+                            WaveRectangleView(title: "Альфа-волны", tag: "Сознательное", tag2: "Бодрствование", image: "WaveImage")
+                                .id(index)
+                                .frame(width: UIScreen.main.bounds.width) // Каждый элемент занимает всю ширину экрана
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollTargetBehavior(.paging)
+                .onChange(of: currentPage) { _, newPage in
+                    withAnimation {
+                        proxy.scrollTo(newPage, anchor: .center)
+                    }
+                }
+                .onAppear {
+                    proxy.scrollTo(currentPage, anchor: .center)
+                }
+                .onChange(of: currentPage) { oldValue, newValue in
+                    print("Current page: \(newValue)")
+                }
             }
             HStack(spacing: 8) {
                 ForEach(0..<3, id: \.self) { index in
                     Circle()
-                        .frame(width: 8, height: 8)
-                        .foregroundColor(currentPage == index ? .blue : .gray.opacity(0.5)) 
+                        .frame(width: currentPage == index ? 17 : 15, height: currentPage == index ? 17 : 15)
+                        .foregroundColor(currentPage == index ? .black : .clear)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.black, lineWidth: currentPage == index ? 0 : 1)
+                        )
                         .onTapGesture {
                             currentPage = index
                         }
